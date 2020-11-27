eb5b016bdc8cd67ed79d928d43aa480262870608
Benson <bkw5279@psu.edu>
2020-11-27 21:55:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next=None
+
+class LinkedList:
+  def __init__(self):
+    self.head=None
+
+  def push(self,new_data):
+    new_node = Node(new_data)
+    new_node.next=self.head
+    self.head=new_node
+  def addTwoLists(self,first,second):
+    prev=None
+    carry = 0
+    while (first is not None or second is not None):
+      fdata = 0 if first is None else first.data
+      sdata = 0 if second is None else second.data
+      total = carry + fdata + sdata
+
+      carry = 1 if total >= 10 else 0
+
+      total = total%10
+
+      temp=Node(total)
+
+      if self.head is None:
+        self.head =  temp
+      else:
+        prev.next = temp
+      prev = temp
+
+      if first is not None:
+        first = first.next
+      if second is not None:
+        second = second.next
+    if carry !=0:
+      temp.next=Node(carry)
+# Driver code
+first = LinkedList()
+second = LinkedList()
+ 
+# Create first list
+first.push(6)
+first.push(4)
+first.push(9)
+first.push(5)
+first.push(7)
+print("First List is ")
+first.printList()
+ 
+# Create second list
+second.push(4)
+second.push(8)
+print ("\nSecond List is ")
+second.printList()
+ 
+# Add the two lists and see result
+res = LinkedList()
+res.addTwoLists(first.head, second.head)
+print("\nResultant list is ")
+res.printList()
